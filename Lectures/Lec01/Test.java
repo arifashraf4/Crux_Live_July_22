020bd53addd40f45af212946675a911ac7d57b4f
Arif-Ashraf <arif.ashraf@yash.com>
2022-07-29 03:13:17 +0530

    Initial commit

---
+++ Lectures/Lec01/Test.java
@@ -0,0 +1,19 @@
+package Lec01;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.print("Welcome to Coding Blocks");
+        System.out.println("Welcome to Coding Blocks");
+        System.out.print("Bye");
+        System.out.println("hello");
+        System.out.print("hey");
+
+        /* Output:
+        Welcome to Coding BlocksWelcome to Coding Blocks
+        Byehello
+        hey
+        */
+    }
+}
+
---
+++ Lectures/Lec01/Variables.java
@@ -0,0 +1,19 @@
+package Lec01;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        int x_2 = 2;
+        System.out.println(x_2);
+        int a = 9;
+        int b = 7;
+        int c = a + b;
+        System.out.println(c);
+        c = a - b;
+        System.out.println(c);
+        ++a;
+        System.out.println(a);
+        --a;
+        System.out.println(a);
+    }
+}
